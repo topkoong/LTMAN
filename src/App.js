11bcc56b7266d0ca18f7e0435e0615a6417d5b70
topkoong <engineer_top@msn.com>
2019-05-14 03:22:45 -0400

    Removed comment from App component

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 			<div className="app__container">
 				<header className="header">
 					<p>
-						ITMan - Coding assesment
+						ITMan - Coding assessment
 					</p>
 				</header>
 				<div>
